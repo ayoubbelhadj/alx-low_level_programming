8b3cc2c1b1e55fff7a8ead418d7d62d6184a0b11
abelhadj <aubbhj@gmail.com>
2023-07-24 15:32:57 -0700

    strlen

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,10 @@ int _isupper(int c);
 int _isdigit(int c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
 
 
 #endif 
